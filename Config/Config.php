31b53bdb36965740427edb2f1c5d8fec977f681e
EnriqueNieto90 <enrikeniet90@gmail.com>
2026-02-10 10:40:40 +0100

    CONFLICTO RESUELTO

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php
 const base_url = "http://biblioteca.enriquenielor.ieslossauces.es/";
 const host = "localhost";
-const user = "userENLBDBiblioteca";
+const user = "userENLBiblioteca";
 const pass = "672qJ&btbWrKmtd%";
 const db = "DBENLBiblioteca";
 const charset = "charset=utf8";
